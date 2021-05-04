faee021f6a452758fed659b0529ba714e59a66eb
kaeun <attackmood@gmail.com>
2021-05-04 16:38:44 +0900

    커밋 스터디

--- 03_로또/test.java
+++ 03_로또/test.java
@@ -1,5 +1,6 @@
 import java.io.*;
 import java.util.*;
+import java.util.Scanner;
 
 class Test {
     String fileName = "우리반.txt";
@@ -10,6 +11,19 @@ class Test {
     int count;
     int i;
 
+    Scanner s = new Scanner(System.in);
+    void input(){
+        System.out.println("파일이름을 입력해주세요: ");
+        String str = s.next();
+        try{
+            int k =  Integer.parseInt(str);
+            System.out.println("파일이름: "+fileName);
+        }catch(NumberFormatException ne){
+			System.out.println("파일이 없습니당");
+			input();
+        }
+    }
+
     Test(){
         try{
             fr = new FileReader(fileName);
@@ -17,14 +31,12 @@ class Test {
             while((name = br.readLine()) !=null){
                     //System.out.println("이름: "+name);
                     count++;
-                
             }
             //System.out.println("총 "+count+"명 검색 완료!!");
         }catch(FileNotFoundException fe){
 			System.out.println(fileName+"파일을 찾지 못하였습니다");
         }catch(IOException ie){
 		}
-
     }
 
     void saveName(){
@@ -35,34 +47,24 @@ class Test {
             while((name=br.readLine()) !=null){
                 list[i] = name;
                 i++;
-
             } 
         }catch(FileNotFoundException fe){
-           
         }
         catch(IOException ie){
         }
-        
-
-
-    }
-
-    void readName(){
+   }
+   void readName(){
         Random r = new Random();
         int j = r.nextInt(i);
         System.out.println("이름: "+list[j]); 
     }
 
-
     public static void main(String[] args) 
 	{
 		Test test= new Test();
+        test.input();
         test.saveName();
         test.readName();
 	}
-    
-
-
-    
-    
+  
 }
