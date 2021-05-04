7317dff10e3bd0e1c282cb37e5fe82ae9980923a
SOON_freemoa_WIN <tnssha@gmail.com>
2021-05-04 17:39:13 +0900

    test_soon2

--- 03_로또/test_soon.java
+++ 03_로또/test_soon.java
@@ -2,64 +2,63 @@ import java.io.*;
 import java.util.*;
 import java.util.Scanner;
 
-class Test_soon {
-    FileReader fr;
+class test_soon {
+    String fileName; //파일 경로 - 빈 문자열로 선언해둠
+    FileReader fr; 
     BufferedReader br;
-    String list[];
+    String list[]; 
     String name;
     int count;
     int i;
 
     Scanner s = new Scanner(System.in);
-    String input(){ // 결과값을 문자열로 반환하는 함수 input();
+    test_soon(){
         System.out.println("파일이름을 입력해주세요: ");
-        String str = "설정/"+ s.next() + ".txt"; // 파일 경로를 저장함 (설정/{입력한 이름}.txt)
-        return str; // input() 의 결과는 위 파일 경로임
+        String str = s.next();
+        fileName = str; // 입력받은 값을 fileName에 저장함
     }
 
-    void countName(String ad){ // 매개변수 : 임의의 문자열 ad
+    void countName(){
         try{
-            fr = new FileReader(ad); // 파d일 명을 매개변수에서 받음
-			br = new BufferedReader(fr);
-            while((name = br.readLine()) !=null){
-                    count++;
+            fr = new FileReader(fileName); //fileName = "우리반.txt"를 읽는다
+			br = new BufferedReader(fr); //줄 인식??
+            while((name = br.readLine()) !=null){ //메모장이 몇줄인지 확인. 파일의 줄이 비어있지 않으면 실행
+                    //System.out.println("이름: "+name);
+                    count++; //줄 갯수(학생 수)만큼 증가시킴
             }
+            //System.out.println("총 "+count+"명 검색 완료!!");
         }catch(FileNotFoundException fe){
-			System.out.println(ad+"파일을 찾지 못하였습니다");
-            return; // 파일이 없으면 끝냄
+			System.out.println(fileName+"파일을 찾지 못하였습니다");
         }catch(IOException ie){
 		}
     }
 
-    void saveName(String ad){ // 매개변수 : 임의의 문자열 ad
+    void saveName(){ //배열을 생성하고 파일의 이름들을 대입함
         try{
-            fr = new FileReader(ad); // 파일 명을 매개변수에서 받음
+            fr = new FileReader(fileName); 
             br = new BufferedReader(fr);
-            list = new String[count];
+            list = new String[count]; //길이가 count인 빈 배열 선언
             while((name=br.readLine()) !=null){
-                list[i] = name;
-                i++;
+                list[i] = name; //list 배열 i번째 요소에 파일의 i번째 줄을 대입함
+                i++; // 줄 갯수만큼 i를 증가시킴
             } 
-            readName(); // 파일이 있으면 readName 실행함
         }catch(FileNotFoundException fe){
-            return; // 파일이 없으면 끝냄
         }
         catch(IOException ie){
         }
    }
    void readName(){
         Random r = new Random();
-        int j = r.nextInt(i);
+        int j = r.nextInt(i); //0부터 i까지의 수 중에 랜덤으로 한개를 가져옴
         System.out.println("이름: "+list[j]); 
     }
 
     public static void main(String[] args) 
 	{
-		Test_soon test= new Test_soon();
-        String adress = test.input(); // input()의 결과를 문자열 변수 adress에 저장함
-        test.countName(adress);
-        test.saveName(adress);
-        // readName은 saveName에서 실행함
+		test_soon lotto= new test_soon();
+        lotto.countName();
+        lotto.saveName();
+        lotto.readName();
 	}
   
 }
