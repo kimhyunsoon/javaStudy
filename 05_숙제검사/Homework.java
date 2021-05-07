d7306dc92f5c8d5f81671d3d87292f790d51065c
kaeun <attackmood@gmail.com>
2021-05-07 16:34:40 +0900

    커밋 스터디

--- 05_숙제검사/Homework.java
+++ 05_숙제검사/Homework.java
@@ -1,16 +1,29 @@
 import java.io.*;
+import java.util.*;
 
 class Homework {
     
-    String path = "C:/SOO/학생들의공간/04_로또(컬렉션)";
+    String path = "C:/KAEUN/JAVA/05_숙제검사_soon/숙제/1일";
 
     void confirmDir(){
-        File f = new File(path);
-        
+        File f = new File(path); //File class
+        if(f.exists()){ //디렉토리가 존재하면 true, 존재하지 않으면 false
+            pln("존재하는 디렉토리");
+            
+        }else{
+            pln("존재하지 않는 디렉토리");
+        }
 
     }
 
-    void showDirs(){
+    void showDirs(File f){
+        File kids[] = f.listFiles(); //listFiles() 디렉토리의 이름을 배열로 반환. kids 배열에 저장
+        for(File kid: kids){//kids에 있는 요소가 소진될 때까지 반복문을 돌려 kid에 저장
+            if(!kid.isFile){ //isFile() 경로에 있는 파일이 디렉토리면 false로 반환, 
+                String name = kid.getName();
+            }
+
+        }
 
 
 
@@ -22,8 +35,8 @@ class Homework {
     }
 
     public static void main(String[] args) {
-        
-    }
+        Homework test = new Homework();
+        test.confirmDir();}
 
 }
 
