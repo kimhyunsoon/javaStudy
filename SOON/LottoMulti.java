3504327f17f228a7efffc01dfb3a65cb704ae818
SOON_freemoa_WIN <tnssha@gmail.com>
2021-05-11 17:43:21 +0900

    soon_LottoMulti (입력값 문자열 여부확인)

--- SOON/LottoMulti.java
+++ SOON/LottoMulti.java
@@ -45,16 +45,19 @@ class LottoMulti
 	}
 
 	//2. 파일을 읽고 set에 저장
-	// try 삭제_ 상위 findFile()에서 중복되어 삭제함)
 	void readFile(File f){ 
-		fr = new FileReader(f);
-		br = new BufferedReader(fr);
-		String name = "";
-		while((name = br.readLine()) != null){
-			name = name.trim();
-			if(name.length() !=0){
-				set.add(name);
+		try{
+			fr = new FileReader(f);
+			br = new BufferedReader(fr);
+			String name = "";
+			while((name = br.readLine()) != null){
+				name = name.trim();
+				if(name.length() !=0){
+					set.add(name);
+				}
 			}
+		}catch(FileNotFoundException fe){
+		}catch(IOException ie){
 		}
 	}
 
@@ -72,16 +75,26 @@ class LottoMulti
 	Scanner s = new Scanner(System.in);
 	void readCount(){
 		System.out.print("당첨자수를 입력하세요 : ");
-		count = s.nextInt();
-		if(count==0){
-			pln("최소한 1명은 뽑아주라구!");
+		String input = s.nextLine();
+		
+		// 추가된 메쏘드_Scanner로 받은 문자가 숫자(정수)인지 확인
+		boolean numCheck = true;
+		numberCheck(input, numCheck); 
+		
+		if(!numCheck) { // numberCheck(); 에서 저장한 numCheck가 false이면 실행함
+			pln("당첨자수는 숫자만 입력해주세요!");
 			readCount();
-		}
-		else if(set.size()<count){
-			pln("당첨자수는 " +set.size()+ " 이하로 입력해주세요!");
-			readCount();
-		} else{
-			readName();
+		} else {
+			count = Integer.parseInt(input); // 숫자인지 확인을 위해 String으로 받았던 입력값을 int로 변환함
+			if(count<=0){
+				pln("최소한 1명은 뽑아주라구!");
+				readCount();
+			}else if(set.size()<count){
+				pln("당첨자수는 " +set.size()+ " 이하로 입력해주세요!");
+				readCount();
+			} else {
+				readName();
+			}
 		}
 	}
 
@@ -109,6 +122,17 @@ class LottoMulti
 		}
 		pln("");
 		System.out.println(count+"분 축하합니다!");
+	}
+
+
+	//추가된 메쏘드_Scanner로 받은 문자가 숫자(정수)인지 확인
+	void numberCheck(String a, Boolean b){ // 매개변수로 문자열(입력값) 과 불리언(체크용)을 받음
+		for (int i =0; i<a.length(); i++) { // 매개변수 입력값의 문자열 길이 만큼 실행
+			char tmp = a.charAt(i); // 매개변수 입력값의 i번째 문자를 tmp에 저장
+			if(!Character.isDigit(tmp)){ // tmp가 숫자가 아니면
+				b = false; // 매개변수 불리언(체크용)을 false로 바꿈
+			} 
+		}
 	}
 
 	void pln(String str){
