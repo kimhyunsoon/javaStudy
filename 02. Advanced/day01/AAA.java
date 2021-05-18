f41360118cf0772b25f9fe9dc765c0495fab42bb
kaeun <attackmood@gmail.com>
2021-05-19 00:48:57 +0900

    커밋 스터디

---
+++ 02. Advanced/day01/AAA.java
@@ -0,0 +1,24 @@
+import java.io.*;
+
+
+public class AAA {
+    //BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    BufferedReader br;
+
+    AAA(){
+        InputStream is= System.in; //표준입력
+        
+    }
+
+    void rw(){
+        
+    }
+
+    
+
+    public static void main(String[] args)  {
+        AAA aaa = new AAA();
+        
+    }
+
+}
--- 02. Advanced/day01/EE.java
+++ 02. Advanced/day01/EE.java
@@ -1,46 +1,41 @@
 import java.io.*;
-//파일에서 파일로
+//파일에서 모니터로
+//주제: FileInputStream & FileOutputStream
 
-public class DD {
+public class EE {
     InputStream is;
     OutputStream os;
     String src = "keyword_kaeun.txt";
-    String dest = "DDtest.txt";
-    
-    DD(){
-        try {
+
+    EE(){
+       try {
             is = new FileInputStream(src);
-            os = new FileOutputStream(dest);
+            os = System.out;
         } catch (FileNotFoundException fe) {
+           //TODO: handle exception
         }
     }
 
     void rw(){
-        byte[] bs = new byte [512];
+        byte bs[] = new byte[512];
         int count = 0;
         try {
-            while ((count = is.read(bs)) != -1) {
+            while ((count = is.read(bs)) !=-1) {
                 os.write(bs, 0, count);
             }
-            System.out.println("복사성공!");
             os.flush();
         } catch (IOException ie) {
         } finally{
             try {
                 is.close();
                 os.close();
-            } catch (IOException ie) {
+            } catch (Exception e) {
             }
         }
-        
     }
 
     public static void main(String[] args) {
-        DD dd = new DD();
-        dd.rw();        
-        
+        EE ee = new EE();
+        ee.rw();
     }
-
-
-
 }
