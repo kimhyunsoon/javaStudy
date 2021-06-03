3947f364d77145bb5a55b043e3c782e78cc19bce
kaeun <attackmood@gmail.com>
2021-06-03 23:35:21 +0900

    0603 서버 코딩

---
+++ project/src/EcardGame/GameServer.java
@@ -0,0 +1,41 @@
+package project.EcardGame;
+
+import java.io.*;
+import java.net.*;
+import java.util.*;
+
+import project.EcardGame.GameThread;
+
+public class GameServer extends Thread{
+
+    ServerSocket ss;
+    Socket sc;
+    public static final int port = 3000;
+    public static final int maxclient = 2;
+    GameThread client;
+
+    GameServer(){
+        //家南 积己
+        try {
+            ss = new ServerSocket(port);
+            while(true){
+                sc = ss.accept();
+                client = new GameThread(sc);
+                client.start();
+            }
+        } catch (IOException ie) {
+        } finally{
+            try {
+                if(ss !=null) ss.close();
+            } catch (IOException ie) {
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        new GameServer();
+
+        
+    }
+
+}
