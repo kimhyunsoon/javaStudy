686aa6ef988e3351932371bfeffcd08a15511b37
SOON <tnssha@gmail.com>
2021-06-29 20:59:54 +0900

    파이썬 디렉토리 삭제 연습

---
+++ SOON/python/test2.py
@@ -0,0 +1,42 @@
+import shutil, os
+
+def deleteRe(dir):
+    if os.path.isfile(dir) :
+        os.remove(dir);
+    elif(os.path.isdir(dir)):
+        list = os.listdir(dir);
+        for i in list :
+            deleteRe(dir+'/'+i);
+        os.rmdir(dir)
+
+            
+def deleteDir():
+    print("삭제하실 디렉토리를 입력해주세요. : ", end='')
+    text = input();
+    text = text.strip();
+    if os.path.isfile(text) or os.path.isdir(text) :
+        print("정말 삭제할까요? (y/n) : ", end='');
+        yn = input();
+        if yn.strip() == 'y' :
+            if os.path.isfile(text) :
+                os.remove(text);
+            else :
+                deleteRe(text)
+        elif yn.strip() == 'n':
+            print("프로그램을 종료합니다.")
+        else :
+            print("잘못된 입력입니다. 프로그램을 종료합니다.")
+    else :
+        print("잘못된 경로입니다. 다시 입력하시겠습니까? (y/n): ", end ='')
+        yn2 = input();
+        if yn2.strip() =='y' :
+            deleteDir();
+        elif yn2.strip() == 'n':
+            print("프로그램을 종료합니다.")
+        else :
+            print("잘못된 입력입니다. 프로그램을 종료합니다.")
+deleteDir();
+
+
+
+
