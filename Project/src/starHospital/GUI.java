1a08e43a196ab415ebc6632527b5d49edc28a80b
kaeun <attackmood@gmail.com>
2021-07-03 09:45:08 +0900

    커밋 스터디

--- Project/src/starHospital/GUI.java
+++ Project/src/starHospital/GUI.java
@@ -2,6 +2,8 @@
 //package starHospital;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.*;
 import java.awt.event.*;
 
@@ -12,14 +14,24 @@ public class GUI extends JFrame{
     JPanel cp;
     public static CardLayout cLayout = new CardLayout();
     JButton regisBtn, serchBtn, resrvBtn, medicBtn, recptBtn, staffBtn;
-    JButton mRenter,mEnter, dRenter, dEnter; 
+    JButton mRenter,mSave, mEnter, dRenter, dSave, dEnter; 
+    JButton msearch, dsearch, mAll, dAll;
     JTextField mNumber, mPhone, mName, mAddr;
     JTextField dNumber, dName, dKinds, dAge, dWeight, dSex;
+    JTextField mInputSearch, dInputSearch;
+    JTable mJTable, dJTable;
 
     JPanel register, search, reserve, medical, receipt, staff;
-    JPanel mData, dData;
+    JPanel mData, dData, mtableP, dtableP, updateP, dupdateP;
     JPanel cardP, centerP;
     JLabel label;
+    JTabbedPane tabs;
+
+    String mcolNames[] = {"회원번호","연락처","반려인 성함", "주소"};
+    String dcolNames[] = {"반려견번호","반려견이름","견종", "연령", "무게", "성별", "최초방문일", "회원번호"};
+    DefaultTableModel mModel = new DefaultTableModel(mcolNames, 0); //회원 테이블
+    DefaultTableModel dModel = new DefaultTableModel(dcolNames, 0); //회원 테이블
+    JComboBox cBox1, cBox2;
 
 
     GUI(){
@@ -101,6 +113,7 @@ public class GUI extends JFrame{
         //회원정보입력
         mData = new JPanel();
         mData.setLayout(new BorderLayout(0,30));
+        
         JPanel mtop = new JPanel();
         mtop.setLayout(new FlowLayout(FlowLayout.LEFT));
         JLabel topinfo = new JLabel("회원정보입력");
@@ -143,12 +156,15 @@ public class GUI extends JFrame{
 
 
         JPanel msouth = new JPanel();
-        msouth.setLayout(new FlowLayout(FlowLayout.CENTER, 30,0));
+        msouth.setLayout(new FlowLayout(FlowLayout.CENTER, 10,0));
         mRenter = new JButton("재입력");
+        mSave = new JButton("저장");
         mEnter = new JButton("등록");
         mRenter.setFont(new Font("맑은 고딕", Font.BOLD, 14));
+        mSave.setFont(new Font("맑은 고딕", Font.BOLD, 14));
         mEnter.setFont(new Font("맑은 고딕", Font.BOLD, 14));
         msouth.add(mRenter);
+        msouth.add(mSave);
         msouth.add(mEnter);
 
 
@@ -219,12 +235,15 @@ public class GUI extends JFrame{
 
 
         JPanel dsouth = new JPanel();
-        dsouth.setLayout(new FlowLayout(FlowLayout.CENTER, 30,0));
+        dsouth.setLayout(new FlowLayout(FlowLayout.CENTER, 10,0));
         dRenter = new JButton("재입력");
+        dSave = new JButton("저장");
         dEnter = new JButton("등록");
         dRenter.setFont(new Font("맑은 고딕", Font.BOLD, 14));
+        dSave.setFont(new Font("맑은 고딕", Font.BOLD, 14));
         dEnter.setFont(new Font("맑은 고딕", Font.BOLD, 14));
         dsouth.add(dRenter);
+        dsouth.add(dSave);
         dsouth.add(dEnter);
         
         dData.add(dtop, BorderLayout.NORTH);
@@ -237,10 +256,154 @@ public class GUI extends JFrame{
         register.add(dData);
 
 
-        
 
-
+        //2. 회원관리 화면
         search = new JPanel();
+        search.setBorder(BorderFactory.createEmptyBorder(0 , 0 , 10 , 10)); //투명 테두리
+        search.setLayout(new BoxLayout(search, BoxLayout.Y_AXIS));
+
+        //2-1. 회원정보조회 (JTable)
+        //회원 테이블 전체 구역
+        mtableP = new JPanel(); 
+        mtableP.setLayout(new BorderLayout(0,0));
+
+        //검색창 구역
+        JPanel mNorth = new JPanel(); 
+        mNorth.setLayout(new BorderLayout(0,0));
+
+        JLabel northTop = new JLabel("회원조회");
+        northTop.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+        mNorth.add(northTop, BorderLayout.NORTH);
+
+        JPanel northBot = new JPanel();
+        northBot.setLayout(new BoxLayout(northBot, BoxLayout.X_AXIS));
+        cBox1 = new JComboBox<String>(mcolNames); //콤보박스
+        mInputSearch = new JTextField("",30);
+        msearch = new JButton(" 검색 ");
+        mAll = new JButton("전체보기");
+        northBot.add(cBox1);
+        northBot.add(mInputSearch);
+        northBot.add(msearch);
+        northBot.add(mAll);
+        mNorth.add(northBot, BorderLayout.CENTER);
+
+
+        //결과창 구역
+        JPanel mCenter = new JPanel(); //테이블부분
+        mCenter.setLayout(new BoxLayout(mCenter, BoxLayout.Y_AXIS));
+        mJTable = new JTable(mModel);
+        JScrollPane sp = new JScrollPane(mJTable);
+        mCenter.add(sp);
+
+        mtableP.add(mNorth, BorderLayout.NORTH);
+        mtableP.add(mCenter, BorderLayout.CENTER);
+
+        //2-2. 환자정보조회 (JTable)
+        //환자 테이블 전체 구역
+        dtableP = new JPanel(); 
+        dtableP.setLayout(new BorderLayout(0,0));
+
+        //검색창 구역
+        JPanel dNorth = new JPanel(); 
+        dNorth.setLayout(new BorderLayout(0,0));
+
+        JLabel dnorthTop = new JLabel("환자조회");
+        dnorthTop.setFont(new Font("맑은 고딕", Font.BOLD, 15));
+        dNorth.add(dnorthTop, BorderLayout.NORTH);
+
+        JPanel dnorthBot = new JPanel();
+        dnorthBot.setLayout(new BoxLayout(dnorthBot, BoxLayout.X_AXIS));
+        cBox2 = new JComboBox<String>(dcolNames); //콤보박스
+        dInputSearch = new JTextField("",30);
+        dsearch = new JButton(" 검색 ");
+        dAll = new JButton("전체보기");
+        dnorthBot.add(cBox2);
+        dnorthBot.add(dInputSearch);
+        dnorthBot.add(dsearch);
+        dnorthBot.add(dAll);
+        dNorth.add(dnorthBot, BorderLayout.CENTER);
+
+        //결과창 구역
+        JPanel dCenter = new JPanel(); //테이블부분
+        dCenter.setLayout(new BoxLayout(dCenter, BoxLayout.Y_AXIS));
+        dJTable = new JTable(dModel);
+        JScrollPane sp2 = new JScrollPane(dJTable);
+        dCenter.add(sp2);
+
+        dtableP.add(dNorth, BorderLayout.NORTH);
+        dtableP.add(dCenter, BorderLayout.CENTER);
+
+
+        //2-3. 조회 및 업데이트
+        tabs = new JTabbedPane(JTabbedPane.TOP, JTabbedPane.WRAP_TAB_LAYOUT);
+        updateP = new JPanel();
+        updateP.setLayout(new GridLayout(4,4,20,0));
+        
+        JButton mupBtn = new JButton("기본정보변경");
+        JButton mccBtn = new JButton("취소");
+
+        updateP.add(mNumberLabel);
+        updateP.add(mNumber);
+        updateP.add(mNameLabel);
+        updateP.add(mName);
+        updateP.add(mPhoneLabel);
+        updateP.add(mPhone);
+        updateP.add(mAddrLabel);
+        updateP.add(mAddr);
+        updateP.add(new JLabel());
+        updateP.add(new JLabel());
+        updateP.add(new JLabel());
+        updateP.add(new JLabel());
+        updateP.add(new JLabel());
+        updateP.add(new JLabel());
+
+        updateP.add(mupBtn);
+        updateP.add(mccBtn);
+
+
+        dupdateP = new JPanel();
+        dupdateP.setLayout(new GridLayout(4,4,20,0));
+        
+        JButton dupBtn = new JButton("기본정보변경");
+        JButton dccBtn = new JButton("취소");
+
+
+        dupdateP.add(dNumberLabel);
+        dupdateP.add(dNumber);
+        dupdateP.add(dNameLabel);
+        dupdateP.add(dName);
+        dupdateP.add(dKindsLabel);
+        dupdateP.add(dKinds);
+        dupdateP.add(dWeightLabel);
+        dupdateP.add(dWeight);
+        dupdateP.add(dAgeLabel);
+        dupdateP.add(dAge);
+        dupdateP.add(dSexLabel);
+        dupdateP.add(dSex);
+        dupdateP.add(new JLabel());
+        dupdateP.add(new JLabel());
+        dupdateP.add(dupBtn);
+        dupdateP.add(dccBtn);
+
+
+
+
+
+
+
+        tabs.addTab("회원정보변경", updateP);
+        tabs.addTab("환자정보변경", dupdateP);
+
+
+
+        search.add(mtableP);
+        search.add(dtableP);
+        search.add(tabs);
+
+
+
+
+
         reserve = new JPanel();
         medical = new JPanel();
         receipt = new JPanel();
@@ -257,17 +420,15 @@ public class GUI extends JFrame{
 
         
 
-        centerP = new JPanel(cLayout);
-        cp.add(centerP, BorderLayout.CENTER);
+        //centerP = new JPanel(cLayout);
+        cp.add(search, BorderLayout.CENTER);
 
 
-        centerP.add(register,"1");
-        centerP.add(search,"2");
-        search.setLayout(new BoxLayout(search, BoxLayout.X_AXIS));
-        centerP.add(reserve,"3");
-        reserve.setLayout(new BoxLayout(reserve, BoxLayout.X_AXIS));
-
-
+        // centerP.add(register,"1");
+        // centerP.add(search,"2");
+        // search.setLayout(new BoxLayout(search, BoxLayout.X_AXIS));
+        // centerP.add(reserve,"3");
+        // reserve.setLayout(new BoxLayout(reserve, BoxLayout.X_AXIS));
 
 
 
@@ -279,14 +440,13 @@ public class GUI extends JFrame{
 
 
 
-        // cp.add(westP, BorderLayout.WEST);
         setUI();
 
     }
 
     void setUI(){
 		setTitle("JTable Test Ver 1.0");
-		setSize(960, 540);
+		setSize(960, 580);
 		setVisible(true);
 		setLocationRelativeTo(null);
 	    setResizable(false);
---
+++ Project/src/starHospital/TabPane.java
@@ -0,0 +1,105 @@
+// package starHospital;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class TabPane extends JFrame{
+
+    JTabbedPane tabs; //JTabbedPane
+    JButton jb1, jb2;
+    Container cp;
+    JPanel register, inquiry, reserve, emp, medical;
+    JLabel label;
+
+
+
+
+
+    TabPane(){
+
+
+        // Setting the JTabbedPane Position and Layout as Wrap
+
+
+        tabs = new JTabbedPane(JTabbedPane.LEFT, JTabbedPane.WRAP_TAB_LAYOUT);
+
+        register = new JPanel();
+        inquiry = new JPanel();
+        reserve = new JPanel();
+        emp = new JPanel();
+        medical = new JPanel();
+
+
+
+
+
+
+        label = new JLabel();
+        label.setIcon(new ImageIcon("C:\\KAEUN\\gitStudy\\Project\\src\\starHospital\\register.png"));
+        // label.setBorderPainted(false);
+        // label.setContentAreaFilled(false);
+        // label.setHorizontalTextPosition(JLabel.TRAILING); // Set the text position regarding its icon
+        // label.setIcon(UIManager.getIcon("OptionPane.informationIcon"));
+        
+        tabs.addTab(null, new JPanel());
+        tabs.setTabComponentAt(0, label); // Here set the custom tab component
+        tabs.addTab("회원가입", register);
+        tabs.addTab("조회", inquiry);
+        tabs.addTab("예약관리", reserve);
+        tabs.addTab("직원관리", emp);
+        tabs.addTab("진료카드", medical);
+
+
+
+
+        jb1 = new JButton("확인");
+        jb2 = new JButton("취소");
+
+
+        register.setLayout(new FlowLayout());
+        register.add(jb1);
+        register.add(jb2);
+
+
+
+
+        
+
+
+        // 이미지 넣고 싶을 때
+
+        // tabs.addTab("Welcome",new ImageIcon("resources\\1.png"),mainPanel,"Takes to the welcome page");
+
+        // tabs.addTab(title, icon, component);
+
+
+
+        getContentPane().add(tabs);
+        setUI();
+    }
+
+    void setPanel(){
+
+
+
+
+
+    }
+    
+
+
+    void setUI(){
+		setTitle("JTable Test Ver 1.0");
+		setSize(960, 540);
+		setVisible(true);
+		setLocationRelativeTo(null);
+	    setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+    public static void main(String[] args) {
+        new TabPane();
+        
+    }
+    
+}
